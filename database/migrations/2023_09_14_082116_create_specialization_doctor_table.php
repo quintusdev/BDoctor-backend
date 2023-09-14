31b28f3529d83e3980408dc5768d182d14d717bb
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-14 10:27:08 +0200

    [FEATURES] Create table pivot doctors and specilization

---
+++ database/migrations/2023_09_14_082116_create_specialization_doctor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialization_doctor', function (Blueprint $table) {
+            /* foreign key tabella doctors */
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
+
+            /* foreign key tabella specializations */
+            $table->unsignedBigInteger('specialization_id');
+            $table->foreign('specialization_id')->references('id')->on('specializations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialization_doctor');
+    }
+};
