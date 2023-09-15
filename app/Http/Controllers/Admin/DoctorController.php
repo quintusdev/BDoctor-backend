3dd89e0037318d399e4a3c549ed61081903b43d0
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-15 10:42:46 +0200

    [FEATURES] Insert comment

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -19,15 +19,19 @@ class DoctorController extends Controller
      */
     public function index()
     {
+        // Ottieni l'ID dell'utente attualmente autenticato
         $user_id = Auth::id();
-        $user = Auth::user();
-        $doctors = Doctor::where('user_id', $user_id)->first();
-        $doctor = $user->doctor;
-        /* $doctorDetails = DB::table('users')
-            ->join('doctors', 'users.id', '=', 'doctors.user_id')
-            ->select('doctors.address', 'doctors.phone')
-            ->get(); */
 
+        // Ottieni l'oggetto dell'utente attualmente autenticato
+        $user = Auth::user();
+
+        // Cerca il dottore associato all'utente corrente utilizzando l'ID utente
+        $doctors = Doctor::where('user_id', $user_id)->first();
+
+        // Ottieni il dottore associato all'utente utilizzando la relazione definita nel modello User
+        $doctor = $user->doctor;
+
+        // Restituisci la vista 'admin.doctors.index' passando i dati alla vista
         return view('admin.doctors.index', compact('doctors', 'doctor', 'user_id', 'user'));
     }
 
