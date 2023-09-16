2d1895d1d3f7dc4ee928dfaf55f36dbde4eea7e0
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-16 18:07:13 +0200

    [FEATURES][STYLE] dashboard page in progress

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        // Ottieni l'ID dell'utente attualmente autenticato
+        $user_id = Auth::id();
+
+        // Ottieni l'oggetto dell'utente attualmente autenticato
+        $user = Auth::user();
+
+        return view('admin.dashboard', compact('user'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
         <div class="row justify-content-center">
             <div class="col">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard di') }}</div>
+                    <div class="card-header">{{ __('Dashboard di ') }}{{ $user->name }} {{ $user->surname }}</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -16,8 +16,83 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        
+                        <span><em>Qui nella tua dashboard puoi vedere in modo rapido tutto quello che ti serve.</em></span>
                     </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="row row-cols-1 row-cols-md-2 g-4">
+                                    <div class="col">
+                                        <div class="card">
+                                            <img src="{{ asset('storage/doctor-profile2.png') }}" style="width:200px;"
+                                                class="card-img-top d-flex justify-center" alt="Immagine profilo dottore">
+                                            <div class="card-body d-flex justify-content-center align-items-center">
+                                                <span class="card-title">Profilo</span>
+                                                <p class="card-text">Vedi e gestisci il tuo profilo</p>
+                                            </div>
+                                            <div class="card-footer d-flex justify-content-center">
+                                                <button type="button" title="Vedi profilo"
+                                                    class="btn btn-outline-primary">Gestisci</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <img src="..." class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">This is a longer card with supporting text below as a
+                                                    natural lead-in to additional content. This content is a little bit
+                                                    longer.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <img src="..." class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">This is a longer card with supporting text below as a
+                                                    natural lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <img src="..." class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">This is a longer card with supporting text below as a
+                                                    natural lead-in to additional content. This content is a little bit
+                                                    longer.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- <div class="card">
+                        <div class="container">
+                            <ul class="nav nav-tabs">
+                                <li class="nav-item">
+                                    <a class="nav-link" href=" {{ route('dashboard') }}">Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href=" {{ route('profile') }} ">Profilo</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href=" {{ route('messages') }} ">Messaggi</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href=" {{ route('reviews') }} ">Recensioni</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="" {{ route('statistics') }} ">Statistiche</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
