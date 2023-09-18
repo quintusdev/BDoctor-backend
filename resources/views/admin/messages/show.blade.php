7dc2868621a794dc3ecc2d6f915ee5bb50b1e739
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-18 15:25:54 +0200

    [STYLE] view messagge and review ok

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -5,29 +5,31 @@
         <div class="row">
             <div class="col-12">
                 <div class="col-12 mt-4 d-flex justify-content-between">
-                    {{-- <div>
-                        @if (!empty($doctor->picture))
-                            <img src="{{ asset('storage/public/images' . $doctor->picture) }}" alt="Immagine Profilo"
-                                class="image_show card product_card" style="width: 300px">
-                        @else
-                            <img src="{{ asset('storage/public/images' . $doctor->immagine_predefinita) }}"
-                                alt="Immagine Predefinita">
-                        @endif
-                    </div> --}}
                     {{-- NOME UTENTE --}}
                     <div class="d-flex col-10 align-items-center mt-1">
-                        <h1>Benvenuto {{ $user->name }} {{ $user->surname }}</h1>
+                        <h1>Messaggio di {{ $message->name }} {{ $message->surname }}</h1>
                     </div>
                     {{-- BUTTON CHE RIPORTA ALLA DASHBOARD --}}
                     <div class="d-flex col-2 align-items-center mt-1">
                         <a href="{{ route('admin.doctors.index') }}" class="btn btn-sm btn-primary">Torna alla Dashboard</a>
                     </div>
                 </div>
-                <h1>Messaggio:</h1>
-                <h4>Nome: {{ $message->name }}</h4>
-                <h3>Cognome: {{ $message->surname }}</h3>
-                <h3>E-mail: {{ $message->email }}</h3>
-                <h4>Testo del messaggio: {{ $message->text }}</h4>
+                <div class="row me-5">
+                    <div class="col-12 col-md-4">
+                        <div class="card mt-4">
+                            <div class="card-body">
+                                <div class="card-content text-center">
+                                    <h5 class="mb-4">{{ $message->text }}</h5>
+                                    <hr>
+                                    <small><em>Messaggio inviato il {{ $message->created_at }}</em></small>
+                                    <hr>
+                                    <a href="{{ route('admin.messages.index', $message->id) }}"
+                                        class="btn btn-sm btn-primary">Torna alla lista</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/admin/reviews/show.blade.php
+++ resources/views/admin/reviews/show.blade.php
@@ -16,18 +16,33 @@
                     </div> --}}
                     {{-- NOME UTENTE --}}
                     <div class="d-flex col-10 align-items-center mt-1">
-                        <h1>Benvenuto {{ $user->name }} {{ $user->surname }}</h1>
+                        <h1>Benvenuto {{ $review->name }} {{ $review->surname }}</h1>
                     </div>
                     {{-- BUTTON CHE RIPORTA ALLA DASHBOARD --}}
                     <div class="d-flex col-2 align-items-center mt-1">
                         <a href="{{ route('admin.doctors.index') }}" class="btn btn-sm btn-primary">Torna alla Dashboard</a>
                     </div>
                 </div>
-                <h1>Recensione:</h1>
-                <h4>Nome: {{ $review->name }}</h4>
-                <h3>Cognome: {{ $review->surname }}</h3>
-                <hr>
-                <h4>Recensione: {{ $review->text }}</h4>
+                {{-- SCHEDE VISUALIZZAZIONE MESSAGGIO --}}
+                <div class="row me-5">
+                    <div class="col-12 col-md-4">
+                        <div class="card mt-4">
+                            <div class="card-body">
+                                <div class="card-content text-center">
+                                    <h4 class="mb-4"><strong>{{ $review->name }} {{ $review->surname }}</strong></h4>
+                                    <hr>
+                                    <h5 class="mb-4">{{ $review->vote }}</h5>
+                                    <h5 class="mb-4">{{ $review->text }}</h5>
+                                    <hr>
+                                    <small><em>Recensione del {{ $review->created_at }}</em></small>
+                                    <hr>
+                                    <a href="{{ route('admin.reviews.index', $review->id) }}"
+                                        class="btn btn-sm btn-primary">Torna alla lista</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
