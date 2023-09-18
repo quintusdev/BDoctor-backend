18de30c32da05e60bcd548baa9c001b72195cedd
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-18 15:53:05 +0200

    [FEATURES] add column to vote pivot table

---
+++ database/migrations/2023_09_18_134450_add_columns_to_existing_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vote_doctor', function (Blueprint $table) {
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('email', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vote_doctor', function (Blueprint $table) {
+            //
+        });
+    }
+};
