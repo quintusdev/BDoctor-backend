6817f6f7e3155e6b542a9af9f13bf5cef9d3928a
robertobenfini <roberto.benfini@gmail.com>
2023-09-20 18:09:10 +0200

    [add] collegamento api

---
+++ app/Http/Controllers/Api/DoctorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Doctor;
+
+class DoctorController extends Controller
+{
+    public function index(){
+        $doctors = Doctor::all();
+            return response()->json([
+                'success' => true,
+                'results' => $doctors
+            ]);
+    }
+}
