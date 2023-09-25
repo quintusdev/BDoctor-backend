72e8ef8c08b132a4d6909f998c703e89046480e9
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-25 19:46:46 +0200

    [ADD] rifatti seeder

---
+++ app/Http/Controllers/Api/DoctorController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Doctor;
+
+class DoctorController extends Controller
+{
+    public function index()
+    {
+        $doctors = Doctor::with('user', 'specializations', 'votes', 'reviews')->get();
+
+        return response()->json([
+            'success' => true,
+            'results' => $doctors
+        ]);
+    }
+    public function getSpecializations($doctor_id)
+    {
+        // Utilizza $doctor_id per recuperare le specializzazioni del dottore
+        $doctor = Doctor::find($doctor_id);
+
+        if (!$doctor) {
+            // Gestisci il caso in cui il dottore non sia stato trovato (ad esempio, restituisci un errore 404)
+            return response()->json(['error' => 'Dottore non trovato'], 404);
+        }
+
+        // Ora puoi ottenere le specializzazioni associate a questo dottore
+        $specializations = $doctor->specializations;
+
+        return response()->json([
+            'success' => true,
+            'results' => $specializations,
+        ]);
+    }
+
+    public function search(Request $request)
+    {
+        // Ottieni i parametri di ricerca dal modulo
+        $name = $request->input('name');
+        $specialization = $request->input('specialization');
+        $vote = $request->input('votes');
+        $review = $request->input('reviews');
+
+        // Esegui la ricerca utilizzando i parametri
+        $doctors = Doctor::with('user', 'specializations', 'votes', 'reviews')
+            ->when($name, function ($query) use ($name) {
+                $query->whereHas('user', function ($subquery) use ($name) {
+                    $subquery->where('name', 'like', '%' . $name . '%');
+                });
+            })
+            ->when($specialization, function ($query) use ($specialization) {
+                $query->whereHas('specializations', function ($subquery) use ($specialization) {
+                    $subquery->where('name', $specialization);
+                });
+            })
+            ->when($vote, function ($query) use ($vote) {
+                $query->whereHas('votes', function ($subquery) use ($vote) {
+                    $subquery->where('name', $vote);
+                });
+            })
+
+            ->when($review, function ($query) use ($review) {
+                $query->whereHas('reviews', function ($subquery) use ($review) {
+                    $subquery->where('name', $review);
+                });
+            })
+
+
+            ->get();
+
+
+        // Restituisci i risultati della ricerca come JSON
+        return response()->json([
+            'success' => true,
+            'results' => $doctors,
+        ]);
+    }
+}
