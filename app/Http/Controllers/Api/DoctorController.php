0c46ed0c93f4261576f9cf460167de5147f9ad0c
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-25 18:53:48 +0200

    [ADD] column and api route

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -38,31 +38,46 @@ class DoctorController extends Controller
     }
 
     public function search(Request $request)
-{
-    // Ottieni i parametri di ricerca dal modulo
-    $name = $request->input('name');
-    $specialization = $request->input('specialization');
+    {
+        // Ottieni i parametri di ricerca dal modulo
+        $name = $request->input('name');
+        $specialization = $request->input('specialization');
 
-    // Esegui la ricerca utilizzando i parametri
-    $doctors = Doctor::with('user', 'specializations')
-    ->when($name, function ($query) use ($name) {
-        $query->whereHas('user', function ($subquery) use ($name) {
-            $subquery->where('name', 'like', '%' . $name . '%');
-        });
-    })
-    ->when($specialization, function ($query) use ($specialization) {
-        $query->whereHas('specializations', function ($subquery) use ($specialization) {
-            $subquery->where('name', $specialization);
-        });
-    })
-    ->get();
+        // Esegui la ricerca utilizzando i parametri
+        $doctors = Doctor::with('user', 'specializations')
+            ->when($name, function ($query) use ($name) {
+                $query->whereHas('user', function ($subquery) use ($name) {
+                    $subquery->where('name', 'like', '%' . $name . '%');
+                });
+            })
+            ->when($specialization, function ($query) use ($specialization) {
+                $query->whereHas('specializations', function ($subquery) use ($specialization) {
+                    $subquery->where('name', $specialization);
+                });
+            })
+            ->get();
 
 
-    // Restituisci i risultati della ricerca come JSON
-    return response()->json([
-        'success' => true,
-        'results' => $doctors,
-    ]);
-}
+        // Restituisci i risultati della ricerca come JSON
+        return response()->json([
+            'success' => true,
+            'results' => $doctors,
+        ]);
+    }
 
+    public function show($doctor_id)
+    {
+        // Utilizza $doctor_id per recuperare il dottore con i dati dell'utente associato
+        $doctor = Doctor::with('user')->find($doctor_id);
+
+        if (!$doctor) {
+            // Gestisci il caso in cui il dottore non sia stato trovato (ad esempio, restituisci un errore 404)
+            return response()->json(['error' => 'Dottore non trovato'], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'results' => $doctor,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/doctors', [DoctorController::class, 'index']);
 Route::get('/doctors/search', [DoctorController::class, 'search']);
 Route::get('/doctors/{doctor_id}/specializations', [DoctorController::class, 'getSpecializations']);
-
+Route::get('/doctors/{doctor_id}', [DoctorController::class, 'show']);
