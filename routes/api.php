133905bae5a8dd9401f7ab011eaea469d50f5149
robertobenfini <roberto.benfini@gmail.com>
2023-09-26 18:16:50 +0200

    [bug]

--- routes/api.php
+++ routes/api.php
@@ -27,5 +27,5 @@ Route::get('/doctors/search', [DoctorController::class, 'search']);
 Route::get('/doctors/{doctor_id}/specializations', [DoctorController::class, 'getSpecializations']);
 Route::get('/sponsor-doctor', [App\Http\Controllers\Api\SponsorController::class, 'index']);
 Route::get('/doctors/{doctor_id}', [DoctorController::class, 'show']);
-Route::post('/reviews', [ReviewController::class, 'create']);
+Route::post('/doctors/reviews', [DoctorController::class, 'newReview']);
 Route::post('/votes', [VoteController::class, 'create']);
