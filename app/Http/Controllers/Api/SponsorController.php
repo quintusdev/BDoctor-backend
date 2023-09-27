2e7c16337f4a026e2dd64f783942a724949733c0
Stefano-Bernini <stefanobernini26@gmail.com>
2023-09-27 22:51:36 +0200

    cambio dati endpoint

--- app/Http/Controllers/Api/SponsorController.php
+++ app/Http/Controllers/Api/SponsorController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Doctor;
+use App\Models\User;
+use App\Models\Specialization;
 
 class SponsorController extends Controller
 {
     public function index()
     {
-        // Esegui una query per ottenere tutti i dati dalla tabella ponte "sponsor_doctor"
-        $sponsorDoctorData = DB::table('sponsor_doctor')->get();
+        $data_attuale = date('Y-m-d');
 
-        // Restituisci i dati come risposta JSON
+        // Esegui una query per ottenere i dati desiderati dalle tabelle utilizzando un'unione (join) corretta
+        $sponsorDoctorData = DB::table('sponsor_doctor')
+            ->join('doctors', 'doctors.id', '=', 'sponsor_doctor.doctor_id')
+            ->join('users', 'users.id', '=', 'doctors.user_id')
+            ->join('specialization_doctor', 'specialization_doctor.doctor_id', '=', 'doctors.id')
+            ->join('specializations', 'specializations.id', '=', 'specialization_doctor.specialization_id')
+            ->select('users.name', 'users.surname', 'doctors.picture', 'specializations.name as specialization_name', 'sponsor_doctor.expire_date')
+            ->where('sponsor_doctor.expire_date', '>', $data_attuale)
+            ->get();
+
         return response()->json($sponsorDoctorData);
     }
 }
