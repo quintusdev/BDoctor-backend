a9a3f053078ad7306dad4c0d417e949a2a0c4d87
michelecanini <studio.michelecanini@gmail.com>
2023-10-01 16:12:29 +0200

    [TWEAK] added mobile home end dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+<div class="jumbotron p-5 mb-4 rounded-3">
     <div class="container py-5">
         <h1 class="display-5 fw-bold text-center">
             Benvenuti su BDoctor
         </h1>
         <h3 class="fs-4 text-center">BDoctors è una web app che permette di cercare un medico specialista e chiedere un appuntamento.</h3>
     </div>
+    <div class="content text-center">
+        <img src="https://static.italiaoggi.it/content_upload/img/2335/59/2335590/medici-387052.jpg" alt="Medici" style="width: 600px" class="rounded-5 img-fluid">
+    </div>
 </div>
 
-<div class="content text-center">
-    <img src="https://static.italiaoggi.it/content_upload/img/2335/59/2335590/medici-387052.jpg" alt="Medici" style="width: 600px" class="rounded-5">
-</div>
 @endsection
